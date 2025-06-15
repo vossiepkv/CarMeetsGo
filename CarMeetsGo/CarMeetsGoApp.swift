b2215f1eb113c0396e53edba45847d0844f5f1de
vossiepkv <vossiepkv@gmail.com>
2025-06-15 17:19:52 +1000

    Initial Commit

---
+++ CarMeetsGo/CarMeetsGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarMeetsGoApp.swift
+//  CarMeetsGo
+//
+//  Created by Patrick  Vohs on 15/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct CarMeetsGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarMeetsGoTests/CarMeetsGoTests.swift
@@ -0,0 +1,16 @@
+//
+//  CarMeetsGoTests.swift
+//  CarMeetsGoTests
+//
+//  Created by Patrick  Vohs on 15/6/2025.
+//
+
+import Testing
+
+struct CarMeetsGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
